93ce7e833c970c98f1e5b3becbf049b419061fb3
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-06-08 06:49:26 +0400

    update header with user data

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,6 +4,6 @@ const authController = require("../controllers/auth.controller");
 
 router.post("/sign-up", authController.signup_post);
 router.post("/sign-in", authController.signin_post);
-router.post("/google-sign-in", authController.google_sign_in_post);
+router.post("/google", authController.google_sign_in_post);
 
 module.exports = router;
