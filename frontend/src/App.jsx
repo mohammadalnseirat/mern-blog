93dee7b831eceb88f1134d97a87b9fbd90428457
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-06-09 03:11:26 +0400

    Make dashboard private (protected)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,23 +6,25 @@ import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
 import Dashboard from "./Pages/Dashboard";
 import Projects from "./Pages/Projects";
-import NavBar from './Components/NavBar'
+import NavBar from "./Components/NavBar";
 import FooterCom from "./Components/FooterCom";
-
+import PrivateRoute from "./Components/PrivateRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <NavBar/>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
-      <FooterCom/>
+      <FooterCom />
     </BrowserRouter>
   );
 };
