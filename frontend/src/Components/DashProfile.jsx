3d733f1ea507f17479b01f663d1005eb7334e7fc
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-06-09 04:02:16 +0400

    create dashProfile of the dashboard

--- frontend/src/Components/DashProfile.jsx
+++ frontend/src/Components/DashProfile.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-
+import { Button, TextInput } from "flowbite-react";
+import React from "react";
+import { useSelector } from "react-redux";
 const DashProfile = () => {
+  // get current user:
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>DashProfile</div>
-  )
-}
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center text-3xl tracking-wide italic font-semibold">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <div className="w-32 h-32 self-center cursor-pointer shadow-lg overflow-hidden rounded-full">
+          <img
+            src={currentUser.profilePicture}
+            alt="user"
+            className="w-full h-full rounded-full border-8 border-[#8b8585af] object-cover"
+          />
+        </div>
+        <TextInput
+          type="text"
+          defaultValue={currentUser.username}
+          id="username"
+          placeholder="username"
+        />
+        <TextInput
+          type="email"
+          defaultValue={currentUser.email}
+          id="email"
+          placeholder="Email"
+        />
+        <TextInput type="password" placeholder="********" id="password" />
+        <Button type="submit" gradientDuoTone={"purpleToBlue"} outline>
+          Update
+        </Button>
+      </form>
+      <div className="flex items-center justify-between mt-4">
+        <span className="cursor-pointer text-red-500  hover:text-red-700 transition-all duration-300">Delete Account</span>
+        <span className="cursor-pointer text-red-500  hover:text-red-700 transition-all duration-300">Sign Out</span>
+      </div>
+    </div>
+  );
+};
 
-export default DashProfile
+export default DashProfile;
--- frontend/src/Components/DashSidebar.jsx
+++ frontend/src/Components/DashSidebar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa6";
 import { Sidebar } from "flowbite-react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const DashSidebar = () => {
   const location = useLocation();
@@ -17,14 +17,16 @@ const DashSidebar = () => {
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Sidebar.Item
-            active={tab === "profile"}
-            icon={FaUser}
-            label="User"
-            labelColor="dark"
-          >
-            Profile
-          </Sidebar.Item>
+          <Link to={"/dashboard?tab=profile"}>
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={FaUser}
+              label="User"
+              labelColor="dark"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
           <Sidebar.Item icon={FaArrowRight}>Sign Out</Sidebar.Item>
         </Sidebar.ItemGroup>
       </Sidebar.Items>
